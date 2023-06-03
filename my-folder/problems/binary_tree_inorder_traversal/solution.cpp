fcb8705ed58ddfd56cdf66dbff21340f42eba058
Abhijeet Singh <89408901+PapaOoOpsie@users.noreply.github.com>
2023-06-03 16:09:20 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (cpp)

---
+++ my-folder/problems/binary_tree_inorder_traversal/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode *cur = root, *pre;
+        
+        while(cur){
+            if(cur->left){
+                pre = cur->left;
+                while(pre->right){
+                    pre = pre->right;
+                }
+                
+                pre->right = cur;
+                TreeNode* tmp = cur;
+                
+                cur = cur->left;
+       
+                tmp->left = nullptr;
+            }else{
+                ans.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+        
+        return ans;
+    }
+};
