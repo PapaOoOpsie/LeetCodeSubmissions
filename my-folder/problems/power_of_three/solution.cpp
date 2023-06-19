7887969358d0fdb3b781669bced809c7327dc899
Abhijeet Singh <89408901+PapaOoOpsie@users.noreply.github.com>
2023-06-19 15:48:01 +0000

    Sync LeetCode submission - Power of Three (cpp)

---
+++ my-folder/problems/power_of_three/solution.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        int max_power_of_3 = pow(3, int(log(INT_MAX)/log(3)));
+        return n > 0 && max_power_of_3 % n == 0;
+    }
+};
